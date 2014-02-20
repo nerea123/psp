3abd240de3ceae0cc0ab96b797873f02cfaae258
nerea123 <sweetlolipop@hotmail.es>
2014-02-20 13:29:44 +0100

    cambiado content-type a application/json

--- PJsonServer/src/Server.java
+++ PJsonServer/src/Server.java
@@ -64,12 +64,11 @@ public class Server {
 					
 					
 					out.write("HTTP/1.0 200 OK\r\n");
-				       out.write("Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n");
+				      
 				       out.write("Server: Apache/0.8.4\r\n");
-				       out.write("Content-Type: text/html\r\n");
+				       out.write("Content-Type: application/json\r\n");
 				       out.write("Content-Length: "+json.length()+"\r\n");
-				       out.write("Expires: Sat, 01 Jan 2000 00:59:59 GMT\r\n");
-				       out.write("Last-modified: Fri, 09 Aug 1996 14:21:40 GMT\r\n");
+				      
 				       out.write("\r\n");
 				       //out.write("<TITLE>JSONSERVER</TITLE>");
 				       //out.write("<h1>Obteniendo JSON</h1>");
@@ -128,6 +127,7 @@ public class Server {
 				json="{ \"id\": \""+articulo.getId()+"\",\"articulo\": \""+articulo.getNombre()+"\",\"precio\": \""+articulo.getPrecio()+"\"}";
 				articulo=null;
 			}
+			
 		}
 	
 
